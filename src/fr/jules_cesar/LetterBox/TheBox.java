126f8fa6375a2387f5c5655c8427c9af96b9203e
GAILLARD Nicolas <ngaillard.vosges@gmail.com>
2013-01-08 06:51:34 -0800

    Mise en place des différentes Box

---
+++ src/fr/jules_cesar/LetterBox/TheBox.java
@@ -0,0 +1,14 @@
+package fr.jules_cesar.TheBox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TheBox extends JavaPlugin implements Listener{
+
+	public void onEnable(){
+
+	}
+
+	public void onDisable(){
+
+	}
+}
